1e941e38de86ace3e239840067d2e8ae356fb936
pawel14178 <49752320+pawel14178@users.noreply.github.com>
2024-06-24 19:37:14 +0200

    Use json to retrieve geolocalization

--- CreateLines.py
+++ CreateLines.py
@@ -1,14 +1,26 @@
 import folium
 import pandas as pd
+import json
 
 # Define a function to read addresses and coordinates from an Excel file
 def read_addresses_from_excel(file_path):
     df = pd.read_excel(file_path, engine='openpyxl')
     return df
 
+# Load JSON data from a file
+with open('GeoLocation.json', 'r') as json_file:
+    address_data = json.load(json_file)
+
+# Function to retrieve latitude and longitude
+def get_lat_lon(start_address, address_data):
+    if start_address in address_data:
+        lat_lon = address_data[start_address]
+        return lat_lon['Latitude'], lat_lon['Longitude']
+    else:
+        return "Address not found", "Address not found"
+
+
 # Read addresses and coordinates from the Excel file
-
-
 ##############################################
 #  Document with locations to display on map #
 ##############################################
@@ -21,12 +33,11 @@ m = folium.Map(location=[52.1, 5.3], zoom_start=8)
 
 # Iterate through each row in the DataFrame and plot trips
 for index, row in df.iterrows():
-    start_lat = row['Start Latitude']
-    start_lon = row['Start Longitude']
-    end_lat = row['End Latitude']
-    end_lon = row['End Longitude']
     start_address = row['Start Address']
     end_address = row['End Address']
+
+    start_lat, start_lon = get_lat_lon(start_address, address_data)    
+    end_lat, end_lon = get_lat_lon(end_address, address_data)   
 
     # Add markers for start and end locations
     folium.Marker(
